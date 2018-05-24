bf26a10574c7b0de82beca26695aa6e573f83e43
Conrad Hyler <conrad.hyler@gmail.com>
2018-05-24 13:56:50 -0700

    adding file

--- Scripts/ce1d/ce1d.py
+++ Scripts/ce1d/ce1d.py
@@ -15,13 +15,13 @@ config.read("/home/pi/ce1d-ip.ini")
 relayip = config.get("ip", "ce1dip")
 curTime = str(datetime.now())
 ip = "http://%s:8080/state.xml?relayState=2&pulseTime=5" % relayip
-conn = httplib.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json", 
-urllib.urlencode({ 
-"token": "a98hhVBULCrTmjKQ2hAgbx4V4z4hSe", 
-"user": "ubYLUfY1QTXwKVBLoNikXKgYTfqdZe", 
-"message": "Alarms would ring. %s" % str(sys.argv[1]),
-}), { "Content-type": "application/x-www-form-urlencoded" })
-conn.getresponse()
+#conn = httplib.HTTPSConnection("api.pushover.net:443")
+#conn.request("POST", "/1/messages.json", 
+#urllib.urlencode({ 
+#"token": "a98hhVBULCrTmjKQ2hAgbx4V4z4hSe", 
+#"user": "ubYLUfY1QTXwKVBLoNikXKgYTfqdZe", 
+#"message": "Alarms would ring. %s" % str(sys.argv[1]),
+#}), { "Content-type": "application/x-www-form-urlencoded" })
+#conn.getresponse()
 #print ip
-#urllib2.urlopen(ip).read()
+urllib2.urlopen(ip).read()
