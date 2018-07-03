38ca7840f4ad244720770484351cb5e917a1bad0
Conrad Hyler <conrad.hyler@gmail.com>
2018-07-02 18:31:12 -0700

    fixed but with AM PM

--- Scripts/ce1d/monitor.sh
+++ Scripts/ce1d/monitor.sh
@@ -15,7 +15,7 @@ incidentDPA=$(echo "$incoming2" | awk -v FS="(DPA: |Incident Number:     )" '{pr
 incidentNum=$(echo "$incoming2" | awk -v FS="(Incident #:     |     Incident Name:   )" '{print $2}' | tr -d "\n")
 incidentName=$(echo "$incoming2" | awk -v FS="(     Incident Name:   |\r\r\n)" '{print $2}' | tr -d "\n")
 incidentTime=$(echo "$incoming2" | awk -v FS="(Time:       [0-9][0-9]-[a-zA-Z][a-z][a-z]-[0-9][0-9][0-9][0-9]\/|\r\r\n)" '{print $2}' | tr -d "\n")
-convDate=$(date +"%X" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+convDate=$(date +"%T" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
 convTime=$(echo "$incidentTime" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
 convDif=$(echo $(( convDate - convTime)))
 echo -e "DPA: $incidentDPA Number: $incidentNum Name: $incidentName\n"
